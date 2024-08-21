fc69011ec1e714b848f05f096eb872f7d41c42a1
projeto 1 <129951535+matheusinho21@users.noreply.github.com>
2024-08-21 08:37:29 -0300

    Create fruta.js

---
+++ fruta.js
@@ -0,0 +1,6 @@
+const frutas = ['pera','morango','uva','abacaxi','banana'];
+const frutasMaisculas = [];
+for (let i = 0; i<frutas.length;i++){
+    frutasMaisculas.push(frutas[i].toUpperCase());
+}
+console.log(frutasMaisculas);z1 
