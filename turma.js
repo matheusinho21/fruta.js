e4c3b7b0a3008f6dcb07cdc7d87c02dc48a26ef8
projeto 1 <129951535+matheusinho21@users.noreply.github.com>
2024-08-21 09:00:24 -0300

    Create turma.js

---
+++ turma.js
@@ -0,0 +1,17 @@
+const turma = {
+    nome: '3° TDS',
+    ano: 2024,
+    professor: {
+        nome: 'Leandro',
+        disciplina: 'prog'
+    },
+    aluno:[{
+        nome:'Paulo',
+        idade: 32,
+    },
+    {
+        nome: 'medina',
+        idade: 13,
+}
+]
+}
